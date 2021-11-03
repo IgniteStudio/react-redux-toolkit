89d6c9c8b88d3e186656080aa2f69967aa16b1ce
David Agaybi <davidagaybi@gmail.com>
2021-11-02 22:37:44 -0400

    second commit...first left slice

---
+++ src/components/ReservationCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import '../App'
+import { removeReservation } from "../features/reservationSlice";
+
+interface ReservationCardTypes { 
+    name: string,
+    index: number,
+
+}
+
+export default function ReservationCard({name, index}: ReservationCardTypes) {
+  
+  const dispatch = useDispatch();
+
+  
+  return <div className="reservation-card-container" onClick={() => {
+    dispatch(removeReservation(index));
+  }}>{name}</div>;
+}
